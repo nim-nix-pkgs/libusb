fbc73e55bbf0efc30f9ff12d08131741783f1323
hugosenari <hugosenari@gmail.com>
2022-03-27 18:19:44 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-libusb-master.ref   = "refs/heads/master";
   inputs.src-libusb-master.owner = "nimious";
   inputs.src-libusb-master.repo  = "libusb";
+  inputs.src-libusb-master.dir   = "";
   inputs.src-libusb-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
